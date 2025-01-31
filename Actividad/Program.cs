60f20ef9d943c4405c8ef1684e5f7ee340fabc75
Valentin Arias <81448598+LilWiPiN@users.noreply.github.com>
2025-01-31 11:55:51 +0000

    Actividad Fixed

---
+++ Actividad/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        int resultadoFactorial = Factorial(5);
+        Console.WriteLine(resultadoFactorial);
+    }
+
+    int factorial(float n)
+    {
+        if (n == 0)
+        {
+            console.WriteLine(1);
+        }
+        else
+        {
+            return n * Factorial(N - 1);
+        }
+    }
+  }
+
